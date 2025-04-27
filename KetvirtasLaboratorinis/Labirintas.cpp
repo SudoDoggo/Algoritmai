af82b0cca1096b7166789f9d89a4171dbb2209a7
Mantvydas Liubartas <mantvydas.liubartas@stud.vilniustech.lt>
2025-04-27 13:43:22 +0300

    Padarytas Laboratorinis

--- KetvirtasLaboratorinis/Labirintas.cpp
+++ KetvirtasLaboratorinis/Labirintas.cpp
@@ -6,11 +6,11 @@
 #include <stack>
 
 using namespace std;
-int kiek=0;
-int maxx=-1;
+int kiek = 0;
+int maxx = -1;
 int n;
 int m;
-string map[1002][1002] = {"#"};
+string map[1002][1002] = { "#" };
 queue < pair<int, int>>pos;
 void RoadPrint(vector<char> road, int kiekis, int x, int y)
 {
@@ -20,35 +20,34 @@ void RoadPrint(vector<char> road, int kiekis, int x, int y)
 	stack<char> roadBack;
 	for (int j = road.size() - 1; j >= 0; j--)
 	{
-		//cout<<kiek<<" "<<road[j]<<endl;
-		if (road[j] == 'D' && map[x - 1][y] == to_string(kiek)||map[x - 1][y] == "A")
+		if (road[j] == 'D' && map[x - 1][y] == to_string(kiek) || map[x - 1][y] == "A")
 		{
 			roadBack.push('D');
 			x--;
 			kiek--;
 		}
-		if (road[j] == 'U' && map[x + 1][y] == to_string(kiek)||map[x + 1][y] == "A")
+		if (road[j] == 'U' && map[x + 1][y] == to_string(kiek) || map[x + 1][y] == "A")
 		{
 			roadBack.push('U');
 			x++;
 			kiek--;
 		}
-		if (road[j] == 'R' && map[x][y - 1] == to_string(kiek)||map[x][y - 1] == "A")
+		if (road[j] == 'R' && map[x][y - 1] == to_string(kiek) || map[x][y - 1] == "A")
 		{
 			roadBack.push('R');
 			y--;
 			kiek--;
 		}
-		if (road[j] == 'L' && map[x][y + 1] == to_string(kiek)|| map[x][y + 1] == "A")
+		if (road[j] == 'L' && map[x][y + 1] == to_string(kiek) || map[x][y + 1] == "A")
 		{
 			roadBack.push('L');
 			y++;
 			kiek--;
 		}
 	}
-	while(!roadBack.empty())
+	while (!roadBack.empty())
 	{
-		cout<<roadBack.top();
+		cout << roadBack.top();
 		roadBack.pop();
 	}
 }
@@ -56,15 +55,15 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-    cin >> n >> m;
+	cin >> n >> m;
 	vector<char> road;
 	char temp;
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
 		{
 			cin >> temp;
-			map[i][j]=temp;
-			if(map[i][j]=="A")
+			map[i][j] = temp;
+			if (map[i][j] == "A")
 				pos.push(make_pair(i, j));
 		}
 	int size = 0;
@@ -77,54 +76,54 @@ int main()
 			if (map[pos.front().first][(pos.front().second) + 1] == "B")
 			{
 				road.push_back('R');
-				RoadPrint(road,kiek,(pos.front().first),(pos.front().second)+1);
+				RoadPrint(road, kiek, (pos.front().first), (pos.front().second) + 1);
 				return 0;
 			}
 			if (map[pos.front().first][(pos.front().second) - 1] == "B")
 			{
 				road.push_back('L');
-				RoadPrint(road,kiek,(pos.front().first),(pos.front().second)-1);
+				RoadPrint(road, kiek, (pos.front().first), (pos.front().second) - 1);
 				return 0;
 			}
 			if (map[(pos.front().first) + 1][pos.front().second] == "B")
 			{
 				road.push_back('D');
-				RoadPrint(road,kiek,(pos.front().first) + 1,(pos.front().second));
+				RoadPrint(road, kiek, (pos.front().first) + 1, (pos.front().second));
 				return 0;
 			}
 			if (map[(pos.front().first) - 1][(pos.front().second)] == "B")
 			{
 				road.push_back('U');
-				RoadPrint(road,kiek,(pos.front().first) - 1,(pos.front().second));
+				RoadPrint(road, kiek, (pos.front().first) - 1, (pos.front().second));
 				return 0;
 			}
 			if (map[pos.front().first][(pos.front().second) + 1] == ".")
 			{
-				map[pos.front().first][(pos.front().second) + 1] = kiek + 48;
+				map[pos.front().first][(pos.front().second) + 1] = to_string(kiek);
 				pos.push(make_pair(pos.front().first, (pos.front().second) + 1));
 				road.push_back('R');
 			}
 			if (map[pos.front().first][(pos.front().second) - 1] == ".")
 			{
-				map[pos.front().first][(pos.front().second) - 1] = kiek + 48;
+				map[pos.front().first][(pos.front().second) - 1] = to_string(kiek);
 				pos.push(make_pair(pos.front().first, (pos.front().second) - 1));
 				road.push_back('L');
 			}
 			if (map[(pos.front().first) + 1][pos.front().second] == ".")
 			{
-				map[(pos.front().first) + 1][pos.front().second] = kiek + 48;
+				map[(pos.front().first) + 1][pos.front().second] = to_string(kiek);
 				pos.push(make_pair((pos.front().first) + 1, pos.front().second));
 				road.push_back('D');
 			}
 			if (map[(pos.front().first) - 1][pos.front().second] == ".")
 			{
-				map[(pos.front().first) - 1][pos.front().second] = kiek + 48;
+				map[(pos.front().first) - 1][pos.front().second] = to_string(kiek);
 				pos.push(make_pair((pos.front().first) - 1, pos.front().second));
 				road.push_back('U');
 			}
 			pos.pop();
 		}
 	}
-	cout<<"NO"<<endl;
-    return 0;
+	cout << "NO" << endl;
+	return 0;
 }
